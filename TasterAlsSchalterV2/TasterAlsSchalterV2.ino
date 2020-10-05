aca014b4b5a0506d81a5cafd7abe889642ca3613
Kl0ska <71316060+Kl0ska@users.noreply.github.com>
2020-10-05 16:57:29 +0200

    Kommentare hinzugefügt

--- TasterAlsSchalterV2/TasterAlsSchalterV2.ino
+++ TasterAlsSchalterV2/TasterAlsSchalterV2.ino
@@ -1,27 +1,27 @@
 #include <OneButton.h>
 
 
-OneButton taster(10, true);
+OneButton taster(10, true);  //Erstelle einen "OneButton" mit dem Namen "taster" und weise diesem den Pin 10 zu
 
 bool statusLED = false;
 
 
 void setup()
 {
-  pinMode(5, OUTPUT);
+  pinMode(5, OUTPUT);  //LED ist an Pin 5 angeschlossen
 
 
-  taster.attachClick(Taster_Funktion);
+  taster.attachClick(Taster_Funktion);  //Weise dem taster die Funktion Einzelklick zu und die Bezeichung für die void
 
 }
 
 void loop()
 {
-  taster.tick();
+  taster.tick();  //Abfrage des Tasters je Zyklus
 
   // You can implement other code in here or just wait a while
   delay(10);
-  digitalWrite(5, statusLED);
+  digitalWrite(5, statusLED);  //Schreibe den Zustand der LED für den Ausgang
 }
 
 void Taster_Funktion()
